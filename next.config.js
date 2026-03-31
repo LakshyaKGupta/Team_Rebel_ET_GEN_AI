a823394c4072c0e50f6596893d6838e8e99773b8
Lakshya K. Gupta <lakshyagupta9721@gmail.com>
2026-03-31 20:31:10 +0530

    Fix: Update Prisma migration to use PostgreSQL instead of SQLite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['better-sqlite3', '@prisma/adapter-better-sqlite3', '@prisma/adapter-libsql', '@libsql/client'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('better-sqlite3', '_http_common');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
